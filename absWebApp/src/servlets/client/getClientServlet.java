74b229a1b097b87b298543d2b4bd8954ffcefbdb
nporat <nadavpo@mta.ac.il>
2022-06-23 12:21:33 +0300

    add client servlet

---
+++ absWebApp/src/servlets/client/getClientServlet.java
@@ -0,0 +1,38 @@
+package servlets.client;
+
+import com.google.gson.Gson;
+import customes.Client;
+import engine.Engine;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utils.ServletUtils;
+import utils.SessionUtils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "ClientServlet", urlPatterns = "/Client")
+public class getClientServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        String usernameFromSession = SessionUtils.getUsername(request);
+        Engine systemEngine = ServletUtils.getSystemEngine(getServletContext());
+
+        Client client = systemEngine.getDatabase().getClientByname(usernameFromSession);
+        Gson gson = new Gson();
+        //todo might need to remez to gson that you want a loan list type of json
+        String jsonResponse = gson.toJson(client);
+
+        try (PrintWriter out = response.getWriter()) {
+            out.print(jsonResponse);
+            out.flush();
+        }
+        System.out.println("On login, request URI is: " + request.getRequestURI());
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
